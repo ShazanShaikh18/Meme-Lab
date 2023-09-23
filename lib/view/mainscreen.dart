7f005cf5b2d4813902d2537b823012414e7a607b
Harris <harissayyed9545@gmail.com>
2023-09-23 13:25:42 +0530

    Created Memes App

---
+++ lib/view/mainscreen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:memes_app/controller/fetchmeme.dart';
+import 'package:memes_app/controller/savemydata.dart';
+import 'package:memes_app/view/splashscreen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  String imageUrl = " ";
+  bool isLoading = true;
+  int? memeNo;
+  int targetMeme = 100;
+  @override
+  void initState() {
+    // TODO: implement initState
+    // splashScreen();
+    GetInitMemeNo();
+    updateImg();
+  }
+
+  GetInitMemeNo() async {
+    memeNo = await SaveMyData.fetchData() ?? 0;
+    if (memeNo! > 100) {
+      targetMeme = 500;
+    } else if (memeNo! > 500) {
+      targetMeme = 1000;
+    }
+    setState(() {});
+  }
+
+  void updateImg() async {
+    String getImageUrl = await FetchMeme.fetchNewMeme();
+    setState(() {
+      imageUrl = getImageUrl;
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 120,
+            ),
+            Text('MEME #${memeNo.toString()}',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Target ${targetMeme} Memes'),
+            SizedBox(
+              height: 50,
+            ),
+            isLoading
+                ? Container(
+                    height: 350,
+                    width: 350,
+                    child: Center(
+                      child: SizedBox(
+                        height: 35,
+                        width: 35,
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
+                  )
+                : Container(
+                    height: 350, width: 350, child: Image.network(imageUrl)),
+            SizedBox(height: 60),
+            ElevatedButton(
+                onPressed: () async {
+                  setState(() {
+                    isLoading = true;
+                  });
+                  await SaveMyData.saveData(memeNo! + 1);
+                  GetInitMemeNo();
+                  updateImg();
+                },
+                child: Container(
+                    height: 30,
+                    width: 100,
+                    child: Center(child: Text('More Fun?')))),
+            Spacer(),
+            Text('App Created By', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 3),
+            Text('Harris Sayyed',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/splashscreen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:memes_app/view/mainscreen.dart';
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({super.key});
+
+  @override
+  State<splashScreen> createState() => _splashScreenState();
+}
+
+class _splashScreenState extends State<splashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(
+      Duration(seconds: 3),
+      () {},
+    );
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MainScreen(),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Image.asset('assets/images/meme.png'),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Welcome to Memes World!',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          )
+        ],
+      )),
+    );
+  }
+}
